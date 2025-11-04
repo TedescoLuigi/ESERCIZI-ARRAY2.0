29702bf56338f12c8a5bc189f0bce9d8657c8a9e
TedescoLuigi <tedesco.luigi@franchettisalviani.net>
2025-11-04 15:39:28 +0100

    finito fino 3

--- ESERCIZI-ARRAY2.0/Program.cs
+++ ESERCIZI-ARRAY2.0/Program.cs
@@ -13,6 +13,27 @@ namespace Esercizi_array
                 Console.WriteLine(numeriContrario[i]);
             }
 
+
+
+            int[] numeriPari = { 10, 25, 33, 47, 52, 61, 78, 99 };
+
+            Console.WriteLine("Elementi con indice pari:");
+             for (int i = 0; i < numeriPari.Length; i += 2)
+             {
+                    Console.WriteLine(numeriPari[i]);
+             }
+
+            int[] numDispari = { 10, 25, 33, 47, 52, 61, 78, 99 };
+
+            Console.WriteLine("Elementi dispari del vettore:");
+            for (int i = 0; i < numDispari.Length; i++)
+            {
+                if (numDispari[i] % 2 != 0)
+                {
+                    Console.WriteLine(numDispari[i]);
+
+                }
+            }
         }
-    }
+
 }
