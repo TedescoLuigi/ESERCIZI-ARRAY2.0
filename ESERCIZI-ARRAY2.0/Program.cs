5c0b094d5d76876cb135962ae472820539ff43da
TedescoLuigi <tedesco.luigi@franchettisalviani.net>
2025-11-04 10:51:54 +0100

    Aggiungere i file di progetto.

---
+++ ESERCIZI-ARRAY2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Esercizi_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeriContrario = { 1, 2, 3, 4, 5 };
+
+            for (int i = numeriContrario.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(numeriContrario[i]);
+            }
+
+        }
+    }
+}
