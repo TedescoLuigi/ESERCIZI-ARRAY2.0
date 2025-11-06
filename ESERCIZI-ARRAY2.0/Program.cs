94d9bee5e5c8fcd90ba6a2769884dc983dafeef3
TedescoLuigi <tedesco.luigi@franchettisalviani.net>
2025-11-06 10:05:49 +0100

    esercizo  6

--- ESERCIZI-ARRAY2.0/Program.cs
+++ ESERCIZI-ARRAY2.0/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿
+using System.Numerics;
 
 namespace Esercizi_array
 {
@@ -18,10 +19,10 @@ namespace Esercizi_array
             int[] numeriPari = { 10, 25, 33, 47, 52, 61, 78, 99 };
 
             Console.WriteLine("Elementi con indice pari:");
-             for (int i = 0; i < numeriPari.Length; i += 2)
-             {
-                    Console.WriteLine(numeriPari[i]);
-             }
+            for (int i = 0; i < numeriPari.Length; i += 2)
+            {
+                Console.WriteLine(numeriPari[i]);
+            }
 
             int[] numDispari = { 10, 25, 33, 47, 52, 61, 78, 99 };
 
@@ -34,6 +35,70 @@ namespace Esercizi_array
 
                 }
             }
+
+
+
+            // Esercizio 6
+            Console.WriteLine("---------------------------------------------------------");
+
+            int[] v1 = { 1, 5, -2, 9, 4 };
+            int x = 3;
+            int contaV2 = 0;
+            int contaV3 = 0;
+
+            // Primo ciclo per contare gli elementi per v2 e v3
+            for (int i = 0; i < v1.Length; i++)
+            {
+                if (v1[i] < x)
+                {
+                    contaV2++;
+                }
+                else
+                {
+                    contaV3++;
+                }
+            }
+            // Creazione dei vettori v2 e v3 con le dimensioni corrette
+            int[] v2 = new int[contaV2];
+            int[] v3 = new int[contaV3];
+            int indixV2 = 0, indixV3 = 0;
+
+            for (int i = 0; i < v1.Length; i++)
+            {
+                if (v1[i] < x)
+                {
+                    v2[indixV2] = v1[i];
+                    indixV2++;
+                }
+                else
+                {
+                    v3[indixV3] = v1[i];
+                    indixV3++;
+                }
+            }
+           for(int i = 0; i < v2.Length; i++)
+            {
+               Console.Write($"[ {v2[i]} ]");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < v3.Length; i++)
+            {
+                Console.Write($"[ {v3[i]} ]");
+            }
+            Console.WriteLine();
+
+            Console.Write("---------------------------------------------------------");
+
+
+
+
+
         }
 
+
+
+
+
+
+    }
 }
